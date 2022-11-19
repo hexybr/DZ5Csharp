58ab0cc598193822bb9a4bed0a64c2224fe1c43d
Handbrake brake <handbrake@mail.ru>
2022-11-20 01:15:07 +0300

    Quest36 Complete

---
+++ Quest36/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
+Console.WriteLine("Введите количество элементов массива: ");// Введение числа элементов массива
+int index;
+if (!int.TryParse(Console.ReadLine()!, out index))
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+int[] blessrnd = new int[index];// Основная часть программы. Создание массива
+FillArray(blessrnd);
+PrintArray(blessrnd);
+Counter(blessrnd);
+
+void FillArray(int[] blessrnd)// Заполнение массива случайными положительными трехзначными числами
+{
+    Random rnd = new Random();
+    for (index = 0; index < blessrnd.Length; index++)
+    {
+        blessrnd[index] = rnd.Next(-1000, 1000);
+    }
+
+}
+void PrintArray(int[] blessrnd)// Вывод получившегося массива
+{
+    Console.WriteLine("Полученный массив: ");
+    for (index = 0; index < blessrnd.Length; index++)
+    {
+        Console.Write(blessrnd[index] + " ");
+    }
+    Console.WriteLine();
+}
+void Counter(int[] rnd)
+{
+    int count = 0;
+    for (int i = 1; i < rnd.Length; i = i + 2)
+    {
+        count = count + rnd[i];
+    }
+    Console.WriteLine("Сумма элементов массива с нечетным индексом = " + count);
+}
